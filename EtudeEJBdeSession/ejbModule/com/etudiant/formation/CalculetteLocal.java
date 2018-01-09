f3a3a02355c2fbf9c2448809ac6064bc74a2df0f
Unknown <csuwannapak@gmail.com>
2018-01-09 16:04:46 +0100

    EJB de session stateLess/stateFull

---
+++ EtudeEJBdeSession/ejbModule/com/etudiant/formation/CalculetteLocal.java
@@ -0,0 +1,13 @@
+/*
+ * Cette interface sera utilisée pour exporter des méthodes vers d'autres EJBs
+ * qui partage le même conteneur.
+ * 
+ */
+package com.etudiant.formation;
+
+import javax.ejb.Local;
+
+@Local
+public interface CalculetteLocal {
+
+}
